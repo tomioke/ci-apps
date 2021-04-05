2ca5ee15801bcb0b0a63b985210639e4da29aee2
tomioke <tomiirvan@gmail.com>
2021-04-05 19:47:22 +0700

    update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,7 +13,7 @@
   <body>
 
   <!-- Bagian navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
   <a class="navbar-brand" href="#">SDN SIDAREJA 01</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
